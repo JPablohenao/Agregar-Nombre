d9c528b311a93068d97fb1de31d4ec6f733a15d1
Pablo <106776147+JPablohenao@users.noreply.github.com>
2023-06-29 14:59:25 -0500

    agrega texto en el input

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import './App.css';
 
 function App() {
   const [user, setUser] = useState(['tomas', 'roma', 'juan', 'pablo'])
+  const [inputValue, setInputValue] = useState('');
 
   const handClick = () => {
-    setUser([...user, 'isma']);
+    setUser([...user, (inputValue)]);
   }
 
   return (
     <div className="App">
       <header className="App-header">
 
-        <a>
+        <a >
           Learn React
         </a>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+
         <button onClick={handClick}> añadir texto </button>
         <ul>
           {user.map((usuario) => (
             <li key={usuario}>{usuario}</li>
           ))}
         </ul>
+
 
       </header>
     </div >
@@ -29,3 +37,9 @@ function App() {
 
 export default App;
 //() => (console.log(user))
+// ... (es un operador spread)
+// subir el codigo al git hub 
+// crear un input que al escribir me agregue a lista y que no se agregue a lista si ya esta 
+// que cuando agregue se borre el contenido del input 
+
+
